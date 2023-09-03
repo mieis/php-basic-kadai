0a656f0224d3f801027caaaea8293dadb082daf1
mieis <143199878+mieis@users.noreply.github.com>
2023-09-03 14:53:56 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>7章課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $tamanegi = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($tamanegi);
+        ?>
+    </p>
+</body>
+
+</html>
