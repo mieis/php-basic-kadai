0fb1aaf2216eae53ab4620d4c6e0bcd839806f89
mieis <143199878+mieis@users.noreply.github.com>
2023-12-26 07:51:50 +0900

    Add files via upload

---
+++ kadai_sort/sort.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // ソート関数を定義
+        function sort_2way($array, $order){
+            if ($order == TRUE) {
+                echo "昇順にソートします。<br>";
+                sort($array);
+                foreach ($array as $val) {
+                    echo $val . "<br>";
+                }
+            } else {
+                echo "降順にソートします。<br>";
+                rsort($array);
+                foreach ($array as $val) {
+                    echo $val . "<br>";
+                }
+            }
+        }
+
+        // ソートする配列を宣言
+        $nums = [15, 4, 18, 23, 10 ];
+
+        // ソート関数を呼び出す
+        sort_2way($nums, TRUE);
+        sort_2way($nums, FALSE);
+
+        ?>
+    </p>
+</body>
+
+</html>
